ed8c20c7bba90b9e526367640b2492f465bb17a2
seki <seki@well-ness.co.jp>
2015-02-24 11:13:55 +0900

    1st commit

---
+++ Vagrantfile
@@ -0,0 +1,24 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+
+  config.vm.box = "centos64"
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 35729, host: 35729
+  config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.hostname = "localhost"
+
+  # config.vm.network "public_network"
+
+  config.vm.provision :shell, path: "script/base.sh"
+  config.vm.synced_folder "./documentRoot", "/var/www/html/public_html"
+
+  config.vm.provider "virtualbox" do |vb|
+      vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+      vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+      vb.memory = "1024"
+     # vb.gui = true
+  end
+
+end
---
+++ script/base.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+DONE=/tmp/.init_base
+if [ ! -f ${DONE} ]; then
+
+    # install httpd
+    sudo yum -y install httpd
+    sudo service httpd start
+    sudo chkconfig httpd on
+
+    # iptables off
+    sudo service iptables stop
+    sudo chkconfig iptables off
+
+    # create documentRoot
+    # -> create auto by Vagrantfile...
+    # sudo mkdir -v /var/www/html/public_html
+    # sudo chown vagrant /var/www/html/ -R
+
+    # mod httpd.conf
+    cp -p /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.orig
+    sudo sed -i -e "s|ServerTokens OS|ServerTokens Prod|" /etc/httpd/conf/httpd.conf
+    sudo sed -i -e "s|KeepAlive Off|KeepAlive On|" /etc/httpd/conf/httpd.conf
+    sudo sed -i -e "s|Options Indexes FollowSymLinks|Options FollowSymLinks|" /etc/httpd/conf/httpd.conf
+    sudo sed -i -e "s|DirectoryIndex index.html index.html.var|DirectoryIndex index.html index.php|" /etc/httpd/conf/httpd.conf
+    sudo sed -i -e "s|ServerSignature On|ServerSignature Off|" /etc/httpd/conf/httpd.conf
+    sudo sed -i -e "s|AddDefaultCharset UTF-8|#AddDefaultCharset UTF-8|" /etc/httpd/conf/httpd.conf
+    sudo sed -i -e "s|AllowOverride None|AllowOverride All|" /etc/httpd/conf/httpd.conf
+    sudo sed -i -e 's|DocumentRoot "/var/www/html"|DocumentRoot "/var/www/html/public_html"|' /etc/httpd/conf/httpd.conf
+    sudo sed -i -e 's|<Directory "/var/www/html">|<Directory "/var/www/html/public_html">|' /etc/httpd/conf/httpd.conf
+
+    # mod welcome.conf
+    sudo sed -i -e "s|Options -Indexes|#Options -Indexes|" /etc/httpd/conf.d/welcome.conf
+    sudo sed -i -e "s|ErrorDocument|#ErrorDocument|" /etc/httpd/conf.d/welcome.conf
+
+    sudo service httpd restart
+
+    # install etc...
+    sudo yum install -y nodejs
+    sudo yum install -y npm
+    sudo npm install -g gulp
+
+    sudo yum install -y ruby
+    sudo yum install -y ruby-devel
+    sudo yum install -y rubygems
+    sudo gem install sass
+    sudo gem install compass
+
+    touch ${DONE}
+fi
+
